c83b6c1f2830f37c822124479691dc5c37083d3f
Tristan Leo <tleo@sipgate.de>
2015-11-11 16:34:59 +0100

    removed unused

--- src/main/java/cc/vileda/experiment/rxchain/UserVerticle.java
+++ src/main/java/cc/vileda/experiment/rxchain/UserVerticle.java
@@ -6,11 +6,9 @@ import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.eventbus.EventBus;
 
 public class UserVerticle extends AbstractVerticle {
-	private CreateUserProcess createUserProcess;
-
 	public void start() {
 		EventBus eventBus = vertx.eventBus();
 		EventStore eventStore = new MongoEventStore(vertx, eventBus);
-		createUserProcess = new CreateUserProcess(eventStore);
+		new CreateUserProcess(eventStore);
 	}
 }
